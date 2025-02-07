43fee05dc3d06f901179bbe67717a0eb84790514
Alex <44531619+AvantGov@users.noreply.github.com>
2025-02-07 13:52:39 -0600

    Update upcomingShows.js
    updated with Feb show, and data for pending April show

--- client/src/data/upcomingShows.js
+++ client/src/data/upcomingShows.js
@@ -1,14 +1,36 @@
 export const upcomingShows = [
+    // {
+    //     id: "show_012625",
+    //     title: "MIZZY, drive like i do, sonic daphne, slick",
+    //     date: "Jan 26th",
+    //     time: "7:00p",
+    //     display: true,
+    //     location: "Cardinal Bar",
+    //     city: "Madison, WI",
+    //     information: "$10 (PWYC)",
+    //     url: "https://www.instagram.com/p/DESgQtzO1-3/"
+    // },
     {
-        id: "show_012625",
-        title: "MIZZY, drive like i do, sonic daphne, slick",
-        date: "Jan 26th",
-        time: "7:00p",
+        id: "show_022325",
+        title: "Glittermouth, HUMID, Drive Like I Do, +more",
+        date: "Feb 23rd",
+        time: "6:00p",
         display: true,
-        location: "Cardinal Bar",
-        city: "Madison, WI",
+        location: "VFW Post 2306",
+        city: "Beloit, WI",
         information: "$10 (PWYC)",
-        url: "https://www.instagram.com/p/DESgQtzO1-3/"
-    },
-
+        url: "https://www.instagram.com/p/DFyBOoUhWY2/?img_index=1"
+    },    
+    // {
+    //     id: "show_041225",
+    //     title: "The Disappeared, Drive Like I Do, Adult Book Club",
+    //     date: "Apr 12th",
+    //     time: "7:00p",
+    //     display: true,
+    //     location: "The Rigby",
+    //     city: "Madison, WI",
+    //     information: "$10 (PWYC)",
+    //     url: ""
+    // },
+    
 ]
