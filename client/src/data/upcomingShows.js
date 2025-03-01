6658ffb062a8c16e28ca7ab79b066eca6003ebf3
Alex <44531619+AvantGov@users.noreply.github.com>
2025-02-28 18:44:02 -0600

    Update upcomingShows.js
    updated with information about upcoming shows through June 2025

--- client/src/data/upcomingShows.js
+++ client/src/data/upcomingShows.js
@@ -10,26 +10,48 @@ export const upcomingShows = [
     //     information: "$10 (PWYC)",
     //     url: "https://www.instagram.com/p/DESgQtzO1-3/"
     // },
-    {
-        id: "show_022325",
-        title: "Glittermouth, HUMID, Drive Like I Do, +more",
-        date: "Feb 23rd",
-        time: "6:00p",
-        display: true,
-        location: "VFW Post 2306",
-        city: "Beloit, WI",
-        information: "$10 (PWYC)",
-        url: "https://www.instagram.com/p/DFyBOoUhWY2/?img_index=1"
-    },    
     // {
-    //     id: "show_041225",
-    //     title: "The Disappeared, Drive Like I Do, Adult Book Club",
-    //     date: "Apr 12th",
+    //     id: "show_022325",
+    //     title: "Glittermouth, HUMID, Drive Like I Do, +more",
+    //     date: "Feb 23rd",
+    //     time: "6:00p",
+    //     display: true,
+    //     location: "VFW Post 2306",
+    //     city: "Beloit, WI",
+    //     information: "$10 (PWYC)",
+    //     url: "https://www.instagram.com/p/DFyBOoUhWY2/?img_index=1"
+    // },    
+    {
+        id: "show_041225",
+        title: "The Disappeared, Drive Like I Do, Adult Book Club",
+        date: "Apr 12th",
+        time: "8:00p",
+        display: true,
+        location: "The Rigby",
+        city: "Madison, WI",
+        information: "$10 at the door",
+        url: "https://www.instagram.com/p/DGj-30vtfGF/?img_index=1"
+    },
+    //     {
+    //     id: "show_042325",
+    //     title: "Tummyache US Tour w/ Khamsin, Drive Like I Do + more",
+    //     date: "Apr 23rd",
     //     time: "7:00p",
     //     display: true,
-    //     location: "The Rigby",
+    //     location: "Communication 608",
     //     city: "Madison, WI",
-    //     information: "$10 (PWYC)",
+    //     information: "",
+    //     url: ""
+    // },
+    //     {
+    //     id: "show_061425",
+    //     title: "DESTROY//SLAY, Drive Like I Do, Sonic Daphne, Sex (comma) Fear",
+    //     date: "Jun 14th",
+    //     time: "7:00p",
+    //     display: true,
+    //     location: "Nottingham Co-Op",
+    //     city: "Madison, WI",
+    //     information: "$10 at the door",
     //     url: ""
     // },
     
