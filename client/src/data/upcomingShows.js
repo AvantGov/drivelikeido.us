b3c26ab5bb5deaf98852f55ab92cea24f7f52c02
Alex <44531619+AvantGov@users.noreply.github.com>
2026-02-01 14:48:18 -0600

    Update upcomingShows.js
    updating show calendar with upcoming shows

--- client/src/data/upcomingShows.js
+++ client/src/data/upcomingShows.js
@@ -76,15 +76,27 @@ export const upcomingShows = [
     //     information: "BENEFIT SHOW | $10 Donation",
     //     url: "https://www.instagram.com/p/DLRGPzfMk0y/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
     // }
+    // {
+    //     id: "show_121425",
+    //     title: "Sonic Daphne farewell w/ Hush Now Sweet Halo, Drive Like I Do, Scaled",
+    //     date: "Dec 14th",
+    //     time: "7:00p",
+    //     display: true,
+    //     location: "Cardinal Bar",
+    //     city: "Madison, WI",
+    //     information: "$10 Donation PWYC",
+    //     url: "https://www.instagram.com/p/DRTZuasjmvU/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    // },
     {
-        id: "show_121425",
-        title: "Sonic Daphne farewell w/ Hush Now Sweet Halo, Drive Like I Do, Scaled",
-        date: "Dec 14th",
+        id: "show_031526",
+        title: "bashford w/ drive like i do, scaled, faulting",
+        date: "Mar 15th",
         time: "7:00p",
         display: true,
         location: "Cardinal Bar",
         city: "Madison, WI",
-        information: "$10 Donation PWYC",
-        url: "https://www.instagram.com/p/DRTZuasjmvU/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+        information: "$7 PWYC",
+        url: "",
     },
+    
 ]
